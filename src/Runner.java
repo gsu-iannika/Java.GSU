a0f55fbf17c6885ee9084769f577a642304a947f
Hanna <55740693+iannika@users.noreply.github.com>
2020-02-19 13:40:01 +0300

    Add files via upload

--- src/Runner.java
+++ src/Runner.java
@@ -1,16 +1,15 @@
 import by.gsy.pms.BusinessTrip;
-import by.gsy.pms.Convert;
-
+import by.gsy.pms.Converter;
 
 public class Runner {
     public static void main(String[] args){
-        BusinessTrip trips[] = {
+        BusinessTrip[] trips = {
                 new BusinessTrip("Anton", 250, 5),
                 new BusinessTrip("Andrey", 157, 2),
                 null,
                 new BusinessTrip("Vitya", 512, 7),
                 new BusinessTrip("Alena", 935, 10),
-                new BusinessTrip("Kate", 470, 6),
+                new BusinessTrip("Kate", 120, 6),
                 new BusinessTrip()
         };
 
@@ -19,21 +18,36 @@ public class Runner {
                 trip.show();
             }
         }
+
         trips[trips.length - 1].setTransport(33);
         System.out.println(("Duration = ") + (trips[0].getDays() + trips[1].getDays()));
-        
+
         for (BusinessTrip trip : trips){
             System.out.println(trip);
         }
-        
+
+        //6
         int totalExpenses = 0;
         for (BusinessTrip trip : trips){
             if (trip != null) {
                 totalExpenses += trip.getTotal();
             }
         }
-        System.out.println("сумму общих расходов = " + totalExpenses);
-        
+        System.out.println("сумму общих расходов: " + totalExpenses);
+
+        //7
+        int workerNumber = 0;
+        int workerTotal = 0;
+        for (int i = 0; i < trips.length; i++) {
+            if (trips[i] != null) {
+                int total = trips[i].getTotal();
+                if (total > workerTotal) {
+                    workerNumber = i;
+                    workerTotal = total;
+                }
+            }
+        }
+        System.out.println("" + workerTotal + "" + trips[workerNumber].getAccount());
 
     }
 
