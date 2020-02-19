0b25baa8b6c546f2c225008c38e9b018fc93d168
Hanna <55740693+iannika@users.noreply.github.com>
2020-02-19 20:54:53 +0300

    Add files via upload

--- src/by/gsy/pms/Converter.java
+++ src/by/gsy/pms/Converter.java
@@ -1,8 +1,11 @@
 package by.gsy.pms;
 
+import java.text.DecimalFormat;
+
 public class Converter {
 
     public static String convertMoney(int val) {
-        return val/100 + "." + (val%100)/10 + val%10;
+        double money = Double.parseDouble((val/1000) + "." + (val%1000)/100 + val%100);
+        return new DecimalFormat("#0.00").format(money);
     }
 }
