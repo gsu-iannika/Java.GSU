884a0b34d0577062764e87f16b6a462e44307f5b
Hanna <55740693+iannika@users.noreply.github.com>
2020-02-19 13:41:52 +0300

    Add files via upload

---
+++ src/Runner.java
@@ -0,0 +1,43 @@
+import by.gsy.pms.BusinessTrip;
+import by.gsy.pms.Converter;
+
+public class Runner {
+    public static void main(String[] args){
+        BusinessTrip[] trips = {
+                new BusinessTrip("Anton", 250, 5),
+                new BusinessTrip("Andrey", 157, 2),
+                null,
+                new BusinessTrip("Vitya", 512, 7),
+                new BusinessTrip("Alena", 935, 10),
+                new BusinessTrip("Kate", 120, 6),
+                new BusinessTrip()
+        };
+
+        for (BusinessTrip trip : trips){
+            if (trip != null) {
+                trip.show();
+            }
+        }
+
+        trips[trips.length - 1].setTransport(33);
+        System.out.println(("Duration = ") + (trips[0].getDays() + trips[1].getDays()));
+
+        for (BusinessTrip trip : trips){
+            System.out.println(trip);
+        }
+
+        int totalExpenses = 0;
+        for (BusinessTrip trip : trips){
+            if (trip != null) {
+                totalExpenses += trip.getTotal();
+            }
+        }
+        System.out.println("сумму общих расходов: " + totalExpenses);
+
+
+
+    }
+
+
+
+}
