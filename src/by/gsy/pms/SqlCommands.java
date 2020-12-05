da28f9996fef50c728b1ee46249bd5aa52a81616
Hanna <55740693+iannika@users.noreply.github.com>
2020-12-05 15:43:49 +0300

    Add files via upload

--- src/by/gsy/pms/SqlCommands.java
+++ src/by/gsy/pms/SqlCommands.java
@@ -13,26 +13,24 @@ public class SqlCommands {
         this.statement = connection.createStatement();
     }
 
-    public String displayInformationAboutSouvenirs(int id_manufacturer) throws SQLException {
-        String query = "SELECT souvenirs.name_souvenir, manufacturers.name_manufacturer, souvenirs.release_date, souvenirs.price, manufacturers.country from (souvenirs \n" +
-                "INNER JOIN manufacturers ON souvenirs.id_manufacturer = manufacturers.id_manufacturer) WHERE id_manufacturer = ?;\n";
+    public String displayInformationAboutSouvenirs(int id) throws SQLException {
+        String query = "SELECT souvenir.name_souvenir, manufacturer.name_manufacturer, souvenir.release_date, souvenir.price, manufacturer.country from (souvenir \n" +
+                "INNER JOIN manufacturer ON souvenir.id_manufacturer = manufacturer.id_manufacturer) WHERE manufacturer.id_manufacturer = ?;\n";
 
         PreparedStatement preparedStatement = connection.prepareStatement(query);
-        preparedStatement.setString(1, String.valueOf(id_manufacturer));
+        preparedStatement.setString(1, String.valueOf(id));
         ResultSet resultSet = preparedStatement.executeQuery();
 
         ArrayList<String> result = new ArrayList<>();
 
         while (resultSet.next()){
-            String id = resultSet.getString(1);
-//            String name_souvenir = resultSet.getString(1);
-//            String name_manufacturer = resultSet.getString(2);
-//            String release_date = resultSet.getString(3);
-//            String price = resultSet.getString(4);
-//            String country = resultSet.getString(5);
+            String name_souvenir = resultSet.getString(1);
+            String name_manufacturer = resultSet.getString(2);
+            String release_date = resultSet.getString(3);
+            String price = resultSet.getString(4);
+            String country = resultSet.getString(5);
 
-//            result.add(name_souvenir + "; " + name_manufacturer + "; " + release_date + "; " + price + "; " + country);
-            result.add(id);
+            result.add("\nназвание сувенира: " + name_souvenir + "; " + "производитель: " + name_manufacturer + "; " + "дату выпуска: " + release_date + "; " + "цена: " + price + "; " + "страна производителя: " + country);
 
         }
 
@@ -41,8 +39,8 @@ public class SqlCommands {
 
 
     public String displayInformationAboutSouvenirsInTheProducedCountry(String country) throws SQLException {
-        String query = "SELECT souvenirs.name_souvenir, manufacturers.name_manufacturer, souvenirs.release_date, souvenirs.price, manufacturers.country FROM (souvenirs \n" +
-                "INNER JOIN manufacturers ON souvenirs.id_manufacturer = manufacturers.id_manufacturer) WHERE country = '?'";
+        String query = "SELECT souvenir.name_souvenir, manufacturer.name_manufacturer, souvenir.release_date, souvenir.price, manufacturer.country FROM (souvenir \n" +
+                "INNER JOIN manufacturer ON souvenir.id_manufacturer = manufacturer.id_manufacturer) WHERE manufacturer.country = ?";
 
         PreparedStatement preparedStatement = connection.prepareStatement(query);
         preparedStatement.setString(1, String.valueOf(country));
@@ -56,14 +54,34 @@ public class SqlCommands {
             String release_date = resultSet.getString(3);
             String price = resultSet.getString(4);
 
-
-            result.add(name_souvenir + "; " + name_manufacturer + "; " + release_date + "; " + price );
+            result.add("\nназвание сувенира: " + name_souvenir + "; " + "производитель: " + name_manufacturer + "; " + "дату выпуска: " + release_date + "; " + "цена: " + price);
 
         }
 
         return result.toString();
     }
 
+
+    public String displayInfoAboutManufacturerWithLowerPrice(int price) throws SQLException {
+        String query = "SELECT manufacturer.name_manufacturer, manufacturer.country, souvenir.price FROM (manufacturer \n" +
+                "INNER JOIN souvenir ON souvenir.id_manufacturer = manufacturer.id_manufacturer) WHERE souvenir.price < ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, String.valueOf(price));
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        ArrayList<String> result = new ArrayList<>();
+
+        while (resultSet.next()){
+            String name_manufacturer = resultSet.getString(1);
+            String country = resultSet.getString(2);
+
+            result.add("\nпроизводитель: " + name_manufacturer + "; " + "страна: " + country );
+
+        }
+
+        return result.toString();
+    }
 
 
 }
